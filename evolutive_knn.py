761d1a8271e531ce4f1ae9dc52c8e3522e114e7c
MatheusAN <matheus.agio@hotmail.com>
2016-11-08 15:26:09 -0200

    Refactoring code

--- evolutive_knn.py
+++ evolutive_knn.py
@@ -36,7 +36,7 @@ class EvolutiveKNN:
         classifier.train([[1, 2, 3, 1], [1, 4, 2, 8], [1, 1, 2, 1]], [0, 1, 0])
     """
     def train(self, training_examples, training_labels):
-        population = self._start_population(len(training_labels))
+        self._start_population(len(training_labels))
         # calculo fitness de todos
         # enquanto nao tiver satisfeito condicao de parada
         #   realizo crossovers
@@ -45,16 +45,11 @@ class EvolutiveKNN:
         #   calculo fitness da nova populacao
 
     def _start_population(self, maximum_k):
-        population = []
-        population_size = self.population_size
-        if self.max_k is None:
-            self.max_k = maximum_k
-        max_k = self.max_k
-        max_weight = self.max_weight
-        for _ in xrange(population_size):
-            k = random.randint(1, max_k)
-            weights = [random.choice(range(max_weight)) for _ in xrange(k)]
-            population.append(
-                Individual(k, weights)
-            )
-        return population
+        if self.max_k is None: self.max_k = maximum_k
+        self.population = []
+        for _ in xrange(self.population_size):
+            k = random.randint(1, self.max_k)
+            weights = [
+                random.choice(range(self.max_weight)) for _ in xrange(k)
+            ]
+            self.population.append(Individual(k, weights))
