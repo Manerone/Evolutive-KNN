0e1b356b6e575a12b105153999780c4a0e2638c8
MatheusAN <matheus.agio@hotmail.com>
2016-11-29 19:31:31 -0200

    Chaging main to execute on all datasets

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from evolutive_knn import EvolutiveKNN
-from banknote_loader import BanknoteLoader
+from dataset_loader import DatasetLoader
+import json
+import os
+import sys
+
+NUMBER_OF_EXECUTIONS = 30
 
 
-banknote = BanknoteLoader('./datasets/banknote.data')
-classifier = EvolutiveKNN(banknote.examples, banknote.labels)
-classifier.train()
-print classifier.global_best.k
-print classifier.global_best.weights
-print classifier.global_best.fitness
-print '-------------------'
-print map(lambda x: [x['individual'].fitness, x['generation']], classifier.hall_of_fame)
-print map(lambda x: x.fitness, classifier.best_of_each_generation)
+def datasets():
+    return os.listdir('./datasets/')
+
+
+def print_percent(dataset, current, total):
+    sys.stdout.write("\033[K")
+    percent = int((i+1.0)*100/total)
+    sys.stdout.write("\r" + dataset + ': ' + str(percent) + '%')
+    sys.stdout.flush()
+
+
+def best_accuracies(dataset, accuracies):
+    data = {
+        'accuracies': accuracies,
+        'average': np.mean(accuracies),
+        'standard deviation': np.std(accuracies)
+    }
+    with open('./results/' + dataset + '.json', 'w') as file:
+        json.dump(data, file)
+
+for dataset in datasets():
+    loader = DatasetLoader('./datasets/' + dataset)
+    best_accuracies = []
+    for i in xrange(NUMBER_OF_EXECUTIONS):
+        print_percent(dataset, i, NUMBER_OF_EXECUTIONS)
+        classifier = EvolutiveKNN(loader.examples, loader.labels)
+        classifier.train()
+        best_accuracies.append(classifier.global_best.fitness)
+    print ''
+    save_results(dataset, best_accuracies)
