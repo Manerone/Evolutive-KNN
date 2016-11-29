578363448d035b640681f279c60ef8f7bbee1cb2
MatheusAN <matheus.agio@hotmail.com>
2016-11-29 19:49:53 -0200

    Fixing dataset_loader

--- dataset_loader.py
+++ dataset_loader.py
@@ -4,14 +4,11 @@ class DatasetLoader:
         self.labels = []
         lines = self._read_lines(path)
         for line in lines:
-            self.examples.append(line[0:-1])
+            self.examples.append(list(map(float, line[0:-1])))
             self.labels.append(line[-1])
 
     def _read_lines(self, path):
         lines = []
         for line in open(path):
-            array = line.rstrip('\n').split(',')
-            lines.append(
-                list(map(float, array))
-            )
+            lines.append(line.rstrip('\n').split(','))
         return lines
